7e923966ecf949a56ee2ec46edc7e8bdd34e4c82
datpetr <57681066+datpetr@users.noreply.github.com>
2020-05-08 15:46:57 +0300

    Update distribution_of_roles.py
    коменты + PEP8

--- distribution_of_roles.py
+++ distribution_of_roles.py
@@ -2,10 +2,11 @@ import discord
 from discord import utils
 
 
-TOKEN = "XXXXXXXXXXXXXXXXXX"
+TOKEN = "NzA1MzE3ODkxNTc4Mzk2NzMz.XrA68w.-NJ7DQGiXyS3xby4kTiC38zK7bA"
 # bot token
 
-POST_ID = 705128184533876767 # post id to read reactions from
+POST_ID = 705128184533876767 
+# post id to read reactions from
 
 # roles list according to emotes
 ROLES = {
@@ -41,25 +42,27 @@ class MyClient(discord.Client):
                         <= MAX_ROLES_PER_USER):
                     await member.add_roles(role)
                     print('[SUCCESS] User {0.display_name} has '
-                          'been granted with role {1.name}'.format(member, role))
+                          'been granted with role {1.name}'.
+                          format(member, role))
                 else:
                     await message.remove_reaction(payload.emoji, member)
-                    print('[ERROR] Too many roles for user {0.display_name}'.format(member))
+                    print('[ERROR] Too many roles for user {0.display_name}'.
+                          format(member))
             except KeyError as e:
                 print('[ERROR] KeyError, no role found for ' + emoji)
             except Exception as e:
                 print(repr(e))
 
     async def on_raw_reaction_remove(self, payload):
-        channel = self.get_channel\
-            (payload.channel_id)
+        channel = self.get_channel(payload.channel_id)
         message = await channel.fetch_message(payload.message_id)
         member = utils.get(message.guild.members, id=payload.user_id)
         try:
             emoji = str(payload.emoji)
             role = utils.get(message.guild.roles, id=ROLES[emoji])
             await member.remove_roles(role)
-            print('[SUCCESS] Role {1.name} has been remove for user {0.display_name}'.format(member, role))
+            print('[SUCCESS] Role {1.name} has been remove for user '
+                  '{0.display_name}'.format(member, role))
         except KeyError as e:
             print('[ERROR] KeyError, no role found for ' + emoji)
         except Exception as e:
