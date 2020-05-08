423efffc3953c18afa07e395ffeabd2d6bcda0de
datpetr <57681066+datpetr@users.noreply.github.com>
2020-05-08 15:36:26 +0300

    Бот спикер
    Данный бот умеет:
    - банить
    - разбанивать
    - кикать
    - отправлять музыку в виде фала пользователю
    - мьютить определённого участника
    - отправлять приветствие от кого-либо к кому-либо
    - приветствовать пользователя
    - показывать время 
    - фильтр чата

--- bot_speaker.py
+++ bot_speaker.py
@@ -8,22 +8,35 @@ import os
 client = commands.Bot(command_prefix='.')
 client.remove_command('help')
 bad_words = ['fuck', 'bitch', 'in your mother', 'fuck you']
-# данный массив был написан с целью не оскорбить кого-либо, а избавить сервер от плохих слов
+# данный массив был написан с целью не 
+# оскорбить кого-либо, а избавить сервер от плохих слов
 
-TOKEN = 'XXXXXXXXXXXXXXXXXXXXXXXXXX'
+TOKEN = 'NzA2ODgyMDc0OTM3Nzg2NDA4.XrAukw.BzGFlu2wFklfZDELDmAseBzOP10'
+
+
+# указываем ТОКЕН для точто чтобы подключится к серверу
 
 
 @client.event
 async def on_ready():
     print("Logged in as: " + client.user.name + "\n")
+    # сообщаем о подключении Бота к северу
+    await client.change_presence(status=discord.Status.online,
+                                 activity=discord.Game('.help'))
 
-    await client.change_presence(status=discord.Status.online, activity=discord.Game('.help'))
+
+# удаляем команду .help для дальнейшей работы
 
 
 @client.event
 async def on_command_error(ctx, error):
     if isinstance(error, commands.CommandNotFound):
-        await ctx.send(f'{ctx.author.name}, there is no such command on the server!')
+        await ctx.send(f'{ctx.author.name}, '
+                       f'there is no such command on the server!')
+
+
+# создаем обработчик ошибок
+# который говорит что нету такой команды 
 
 
 @client.event
@@ -32,7 +45,11 @@ async def on_message(message):
     msg = message.content.lower()
     if msg in bad_words:
         await message.delete()
-        await message.author.send(f'{message.author.name}, not to express!')
+        await message.author.send(f'{message.author.name}, '
+                                  f'not to express!')
+
+
+# филт чата
 
 
 @client.event
@@ -42,10 +59,16 @@ async def on_member_join(member):
     await member.add_roles(role)
 
 
+# выдача роли при входе на сервер
+
+
 @client.command(pass_context=True)
 @commands.has_permissions(administrator=True)
-async def clear(ctx, amount:int):
+async def clear(ctx, amount: int):
     await ctx.channel.purge(limit=amount)
+
+
+# очистка чата
 
 
 @client.command(pass_context=True)
@@ -55,54 +78,78 @@ async def hello(ctx, amount=1):
     await ctx.author.send('Hello {}'.format(author.mention))
 
 
+# приветсвие пользователя
+
+
 @client.command(pass_context=True)
 @commands.has_permissions(administrator=True)
 async def ban(ctx, member: discord.Member, *, reason=None):
-    emb = discord.Embed(title='Ban', colour=discord.Colour.red())
-
+    emb = discord.Embed(title='Ban',
+                        colour=discord.Colour.red())
+    # задаём заголовок
     await ctx.channel.purge(limit=1)
+    # удаляем сообщение
     await member.ban(reason=reason)
-
+    # далее просто занимаемся интрефейсом сообщения
     emb.set_author(name=member.name, icon_url=member.avatar_url)
-    emb.add_field(name='Ban user', value='Baned user: {}'.format(member.mention))
-    emb.set_footer(text='Was banned by the administrator {}'.format(ctx.author.name), icon_url=ctx.author.avatar_url)
+    emb.add_field(name='Ban user', value='Baned user: '
+                                         '{}'.
+                  format(member.mention))
+    emb.set_footer(text='Was banned by the administrator {}'.
+                   format(ctx.author.name), icon_url=ctx.author.avatar_url)
     await ctx.send(embed=emb)
+
+
+# бан участника
 
 
 @client.command(pass_context=True)
 @commands.has_permissions(administrator=True)
 async def kick(ctx, member: discord.Member, *, reason=None):
+    # настройка интерфейса сообщения
     emb = discord.Embed(title='Kick', colour=discord.Colour.blue())
     await ctx.channel.purge(limit=1)
     await member.kick(reason=reason)
     emb.set_author(name=member.name, icon_url=member.avatar_url)
-    emb.add_field(name='Kick user', value='Kick user: {}'.format(member.mention))
-    emb.set_footer(text='Was kicked by the administrator {}'.format(ctx.author.name), icon_url=ctx.author.avatar_url)
+    emb.add_field(name='Kick user', value='Kick user: {}'.
+                  format(member.mention))
+    emb.set_footer(text='Was kicked by the administrator {}'.
+                   format(ctx.author.name),
+                   icon_url=ctx.author.avatar_url)
     await ctx.send(embed=emb)
+# кик участника
 
 
 @client.command(pass_context=True)
 @commands.has_permissions(administrator=True)
 async def unban(ctx, *, member):
     await ctx.channel.purge(limit=1)
+    # удаляем сообщение
     banners_users = await ctx.guild.bans()
+    # получаем список забаненых участников
+    # ищем нужного участника
     for i in banners_users:
         user = i.user
         await ctx.guild.unban(user)
-        await ctx.send("Unabanned user {}".format(user.mention))
+        # разбаниваем участника
+        await ctx.send("Unabanned user {}".
+                       format(user.mention))
         return
 
+
+# анбан участника
 
 @client.command(pass_context=True)
 async def help(ctx):
     author = ctx.message.author
     emb = discord.Embed(title="Server commands:")
-    
+    # тут мы выводим все команды сервера
     emb.add_field(name='.hello', value='A greeting to the user')
     emb.add_field(name='.clean', value='Clearing messages')
     emb.add_field(name='.kick', value='Deleting a participant from the server')
     emb.add_field(name='.ban', value='Restricting access to the server')
-    emb.add_field(name='.unban', value='The removal of restrictions to the server')
+    emb.add_field(name='.unban',
+                  value='The removal of restrictions to the server')
     emb.add_field(name='.time', value='Time and date')
     emb.add_field(name='.mute', value='Mute user')
     emb.add_field(name='.send_msg', value='Send greetings')
@@ -110,46 +157,74 @@ async def help(ctx):
     await ctx.author.send(author, embed=emb)
 
 
+# команда help
+
 @client.command(pass_context=True)
 async def time(ctx):
-    emb = discord.Embed(title='Exact time', description='Click on "Exact time" to find out the exact time', colour=discord.Color.blue(), url='https://www.timeserver.ru')
-
+    emb = discord.Embed(title='Exact time',
+                        description='Click on '
+                                    '"Exact time" to find out the exact time',
+                        colour=discord.Color.blue(),
+                        url='https://www.timeserver.ru')
+    # задаем заголовок
     emb.set_author(name=client.user.name, icon_url=client.user.avatar_url)
-    emb.set_footer(text='Thanks, for using our bot!', icon_url=ctx.author.avatar_url)
-    emb.set_image(url='https://yt3.ggpht.com/a/AGF-l7__Vf7dLdr-fCTm0dN4GbnBMVoTjm4MOWmatg=s900-c-k-c0xffffffff-no-rj-mo')
-    emb.set_thumbnail(url='https://yt3.ggpht.com/a/AGF-l7__Vf7dLdr-fCTm0dN4GbnBMVoTjm4MOWmatg=s900-c-k-c0xffffffff-no-rj-mo')
-
-    emb.add_field(name="Time and date:", value='{}'.format(datetime.datetime.now()))
-
+    # задаем имя и аву
+    emb.set_footer(text='Thanks, for using our bot!',
+                   icon_url=ctx.author.avatar_url)
+    # задаём текст к аве
+    emb.set_image(url='https://yt3.ggpht.com/a/AGF-l7__'
+                      'Vf7dLdr-fCTm0dN4GbnBMVoTjm4MOWmatg'
+                      '=s900-c-k-c0xffffffff-no-rj-mo')
+    # задаем картинку по ссылке
+    emb.set_thumbnail(url='https://yt3.ggpht.com/a/AGF-l7__'
+                          'Vf7dLdr-fCTm0dN4GbnBMVoTjm4MOWmatg='
+                          's900-c-k-c0xffffffff-no-rj-mo')
+    # задаем картинку по ссылке
+    emb.add_field(name="Time and date:", value='{}'.
+                  format(datetime.datetime.now()))
+    # печатаем время
     await ctx.author.send(embed=emb)
 
+
+# выводим время
 
 @client.command()
 @commands.has_permissions(administrator=True)
 async def mute(ctx, member: discord.Member):
     await ctx.channel.purge(limit=1)
+    # удаляем последнее сообщение
     mute_role = discord.utils.get(ctx.message.guild.roles, name='MUTE')
-    
+    # вытаскиваем роль
     await member.add_roles(mute_role)
-    await ctx.send('From {}, the restriction of chat, for violations of the rights of the user!'.format(member.mention))
+    # добавляем роль
+    await ctx.send('From {}, the restriction of chat, '
+                   'for violations of the rights '
+                   'of the user!'.format(member.mention))
 
+
+# выводим на печать сообщения
+# команда запрещения сообщений
 
 @client.command()
 async def send_msg(ctx, member: discord.Member):
     await member.send(f'{member.name}, greetings from {ctx.author.name}')
 
 
+# отправляем в личку приветствие
+
 @client.command()
 async def music(ctx, url: str):
     song_there = os.path.isfile('song.mp3')
+    #
     try:
         if song_there:
             os.remove('song.mp3')
             print('[log] Старый файл удалён')
     except PermissionError:
         print('[log] Не удалось удалить файл')
-        
+    # обработка ошибок
     await ctx.send('Please wait')
+    # посылаем сообщение
     ydl_opts = {
         'format': 'bestaudio/best',
         'postprocessors': [{
@@ -158,19 +233,26 @@ async def music(ctx, url: str):
             'preferredquality': '192'
         }],
     }
-
+    # задаём праметры для музыки
     with youtube_dl.YoutubeDL(ydl_opts) as ydl:
         print('[log] Загружаю музыку...')
         ydl.download([url])
-
+    # загружаем музыку с YouTube
     for file in os.listdir('./'):
+        # ищем файл с раширанием mp3
         if file.endswith('.mp3'):
             name = file
             print('[log] Высылаю файл {}'.format(file))
             os.rename(file, 'song.mp3')
-    await ctx.send(file=discord.File(open('song.mp3', "rb"), name))
+            # переименовыем файл 
+    await ctx.author.send(file=discord.
+                          File(open('song.mp3',
+                                    "rb"), name))
 
 
+# выслылаем музыку лично пользователю
+
+# обработка ошибок на подобии отсуствуют права или не указан аргумент
 @music.error
 async def music_error(ctx, error):
     if isinstance(error, commands.MissingRequiredArgument):
@@ -218,3 +300,4 @@ async def mute_error(ctx, error):
 
 
 client.run(TOKEN)
+# запуск бота
