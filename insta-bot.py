adcf5a345873947cc40dad8e964f5bb4bace3fa3
crja73 <57744842+crja73@users.noreply.github.com>
2020-07-05 19:40:09 +0300

    Add files via upload

--- insta-bot.py
+++ insta-bot.py
@@ -1,11 +1,11 @@
 from instapy import InstaPy
 
-# username = input('Введите ваше имя в insagram =>')
-# password = input('Введите пароль =>')
-# hashtag = input('введите хештег =>')
+username = input('Введите ваше имя в insagram =>')
+password = input('Введите пароль =>')
+hashtag = input('введите хештег =>')
 def bot():
 	try:
-		session = InstaPy(username='crja73', password='Qwased2003')  #headless_browser=True
+		session = InstaPy(username=str(username), password=str(password))  #headless_browser=True
 		session.login()
 		session.set_quota_supervisor(enabled=True, sleep_after=["likes", "comments_d", "follows", "unfollows", "server_calls_h"], sleepyhead=True, stochastic_flow=True, notify_me=True,
                               peak_likes_hourly=57,
@@ -19,15 +19,18 @@ def bot():
                                  	peak_server_calls_hourly=None,
                                  	peak_server_calls_daily=4700)
 		for i in range(20):
-			session.like_by_tags(['bmw'], amount=5)
+			session.like_by_tags([hashtag], amount=5)
 			session.set_dont_like(["naked", "nsfw"])
-			session.set_do_follow(True, percentage=100)
+			try:
+				session.set_do_follow(True, percentage=100)
+			except:
+				print('Что то пошло не по плану!')
 			try:
 				session.set_comments(['Awesome', 'Really Cool', 'I like your stuff'])
 			except:
-				print('Что то пошло не по пану')
+				print('Что то пошло не по пану!')
 			session.set_do_like(enabled=True, percentage=100)
 	except:
-		print('Возможно вы ввели некорректное имя пользователя или пароль, удостоверьтесь, что у вас установлен Firefox и есть интернет-соединение и попробуйте еще раз!')
+		print('Возможно вы ввели некорректное имя пользователя / пароль или хэштег, удостоверьтесь, что у вас установлен Firefox(обязательно) и есть интернет-соединение и попробуйте еще раз!')
 
 bot()
